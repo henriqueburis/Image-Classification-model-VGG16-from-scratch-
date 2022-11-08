24fc1320a509c4e61cd37e974caa8a5b82ba7ad2
Luiz Henrique Buris <henrique.buris@outlook.com.br>
2022-11-08 18:53:00 -0300

    Add files via upload

--- vgg16.py
+++ vgg16.py
@@ -65,6 +65,6 @@ class vgg16scratch(nn.Module):
         out = self.fc2(out)
         return out
 
-net = VGG('VGG11')
+net = vgg16scratch(n_classe)
 x = torch.randn(2,3,32,32)
 print(net(Variable(x)).size())
